14115bd0702a710a1f6bba2f0aa970ce2476dbdd
philipheferreira <philipheferreira@gmail.com>
2025-03-22 18:17:31 -0300

    Feat: cap 2 create component list

---
+++ 2-to-do-list/src/components/list.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        lista
+    </div>
+</template>
+<script>
+    export default{
+        data() {
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
