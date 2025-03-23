be4f48534f4860a21df37f4ce3bf829789a1d7d2
philipheferreira <philipheferreira@gmail.com>
2025-03-23 06:53:24 -0300

    Feat: cap 2 project

--- 2-to-do-list/src/components/list.vue
+++ 2-to-do-list/src/components/list.vue
@@ -1,10 +1,11 @@
 <template>
-    <div>
+    <div class="container">
         <h2>To-Do List</h2>
-        <input v-model="novaTarefa" @keyup.enter="adicionarTarefas" placeholder="Nova tarefa"/>
+        <input class="receberTarefa" v-model="novaTarefa" @keyup.enter="adicionarTarefas" placeholder="Nova tarefa"/>
+        <button class="botaoMetodoAdicionarTarefas" @click="adicionarTarefas">Adicionar tarefas</button>
         <ul>
-            <li v-for="tarefa in tarefas" :key="tarefa">
-                {{ tarefas }}
+            <li v-for="tarefa in tarefas" :key="tarefa.id">
+                {{ tarefa }}
             </li>
         </ul>
     </div>
@@ -32,5 +33,45 @@
 </script>
 
 <style scoped>
+
+    body {
+        font-family: Arial, sans-serif;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #f4f4f4;
+    }
+
+    .container {
+        background: white;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        width: 300px;
+    }
+
+    .receberTarefa{
+        margin-top: 10px;
+    }
+
+    .botaoMetodoAdicionarTarefas{
+        background: red;
+        color: white;
+        border: none;
+        cursor: pointer;
+        padding: 3px 7px;
+    }
+
+    ul {
+        list-style: none; /* Tira os pontos */
+         padding: 0;
+    }
+    li {
+        display: flex;
+        justify-content: space-between;
+        padding: 5px;
+        border-bottom: 1px solid #ddd;
+    }
 
 </style>
