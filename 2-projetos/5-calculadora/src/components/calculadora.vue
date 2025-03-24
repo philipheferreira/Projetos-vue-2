7d049a09811be90f79895499c3c1f7beed9718f6
philipheferreira <philipheferreira@gmail.com>
2025-03-24 12:14:44 -0300

    Feat: module 2 cap 5 finish calculator project

--- 2-projetos/5-calculadora/src/components/calculadora.vue
+++ 2-projetos/5-calculadora/src/components/calculadora.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-        Calculadora
+        <div class="calculadora">
+            <input type="text" v-model="display" disabled/>
+            <div class="botoes">
+                <button v-for="botao in botoes" :key="botao" @click="pressionar(botao)"> {{ botao }} </button>
+                <button @click="limpar">C</button>
+                <button @click="calcular">=</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,12 +16,62 @@
         name: 'calculadoraComponent',
         data () {
             return {
-
+                display: '',
+                botoes: ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '+']
+            }
+        },
+        methods: {
+            calcular(){
+                try {
+                    this.display = eval(this.display)
+                } catch (e) {
+                    this.display = 'erro'
+                }
+            },
+            limpar(){
+                this.display = ''
+            },
+            pressionar(valor){
+                this.display += valor
             }
         }
+
     }
 </script>
 
 <style scoped>
-
+body {
+            font-family: Arial, sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f4f4f4;
+        }
+        .calculadora {
+            width: 300px;
+            padding: 20px;
+            background: white;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            border-radius: 10px;
+            text-align: center;
+        }
+        input {
+            width: 100%;
+            height: 50px;
+            font-size: 1.5em;
+            text-align: right;
+            margin-bottom: 10px;
+            padding: 5px;
+        }
+        .botoes {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            gap: 5px;
+        }
+        botao {
+            font-size: 1.2em;
+            padding: 15px;
+            cursor: pointer;
+        }
 </style>
