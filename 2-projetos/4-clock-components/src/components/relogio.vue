466218c1173e179d046881c300cdab763bae45ed
philipheferreira <philipheferreira@gmail.com>
2025-03-29 09:32:14 -0300

    Feat: basic functionalite from clock components project

--- 2-projetos/4-clock-components/src/components/relogio.vue
+++ 2-projetos/4-clock-components/src/components/relogio.vue
@@ -1,17 +1,30 @@
 <template>
-
     <div>
-        Relogio
+        {{ hora }} : {{ minuto }} : {{ segundo }}
     </div>
-
 </template>
 
 <script>
 
-    export default {
+    export default{
+        name: 'relogioComponent',
+        mounted() {
+            this.atualizarRelogio() // Atualiza imediatamente ao montar o componente
+                setInterval(this.atualizarRelogio, 1000) // Atualiza a cada segundo
+        },
         data () {
             return{
-
+                hora: '',
+                minuto: '',
+                segundo: ''
+            }
+        },
+        methods: { 
+            atualizarRelogio() {
+                let agora = new Date()
+                this.hora = agora.getHours()
+                this.minuto = agora.getMinutes()
+                this.segundo = agora.getSeconds()
             }
         }
     }
