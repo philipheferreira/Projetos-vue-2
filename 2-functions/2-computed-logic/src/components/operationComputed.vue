f313a664d92e6e646b59b0b2af0157db015947a2
philipheferreira <philipheferreira@gmail.com>
2025-03-30 16:21:05 -0300

    Feat: function computed finish

--- 2-functions/2-computed-logic/src/components/operationComputed.vue
+++ 2-functions/2-computed-logic/src/components/operationComputed.vue
@@ -1,8 +1,21 @@
+<!--
+
+Como funciona?
+Se primeiroNome ou ultimoNome mudar, nomeCompleto será automaticamente atualizado.
+
+Se nomeCompleto for chamado várias vezes sem mudanças nas dependências, ele não será recalculado (por conta do cache).
+
+-->
 
 <template>
     <div>
-        
-    Operação computed
+        <p>No Vue 2, a funcionalidade computed é usada para criar propriedades computadas, que são valores derivados 
+          do estado reativo do componente. Diferente das propriedades normais (data), as propriedades computadas são
+           reavaliadas automaticamente quando suas dependências mudam e são armazenadas em cache, evitando cálculos 
+           desnecessários.</p>
+           <br/>
+        <h3>Operação: </h3>
+        <p>Nome Completo: {{ nomeCompleto }}</p>
         
     </div>
   </template>
@@ -11,6 +24,13 @@
   export default {
     data() {
       return {
+        primeiroNome: "Philiphe",
+        ultimoNome: "Ferreira"
+      }
+    },
+    computed: {
+      nomeCompleto() {
+        return `${this.primeiroNome} ${this.ultimoNome}`
       }
     }
   }
