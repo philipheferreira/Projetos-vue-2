f7f76bee984e1ac30f93ae53452810c9b9b7ff4d
philipheferreira <philipheferreira@gmail.com>
2025-03-31 15:00:52 -0300

    Feat: module 2 cap 7 finish

---
+++ 2-functions/7-created-second-logic/src/components/operationCreated.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p> {{ mensagem }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'createdComponente',
+    created(){
+        fetch("https://jsonplaceholder.typicode.com/users/1")
+            .then(response => response.json())
+            .then(data => {
+            this.usuario = data;
+        });
+    },
+    data () {
+        return{
+            mensagem: "Bem vindo ao Vue"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
