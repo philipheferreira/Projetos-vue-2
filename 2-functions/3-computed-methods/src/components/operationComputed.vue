668fcbf3228e2ae1a5ecb3450b61995c4bb987ae
philipheferreira <philipheferreira@gmail.com>
2025-03-31 08:23:03 -0300

    Feat: function cap 4 finish

--- 2-functions/3-computed-methods/src/components/operationComputed.vue
+++ 2-functions/3-computed-methods/src/components/operationComputed.vue
@@ -1,17 +1,37 @@
+<!--
+
+    🆚 Computed vs Methods
+🔹 Computed: O resultado é armazenado em cache e só recalculado quando necessário.
+🔹 Methods: Sempre executa o código ao ser chamado, sem cache.
+-->
+
 <template>
-
     <div>
-        
-    </div>
+        <h3>Operação: </h3>
+        <p>Nome Completo pelo Computed: {{ nomeCompleto }}</p>
+        <p>Nome Completo pelo Method: {{ nomeCompletoMethodo }}</p>
 
+    </div>
 </template>
 
 <script>
 
     export default {
+        name: 'computedComponent',
+        computed: {
+            nomeCompleto() {
+                return `${this.primeiroNome} ${this.ultimoNome}`
+            }
+        },
         data() {
             return{
-
+                primeiroNome: "Philiphe",
+                ultimoNome: "Ferreira"
+            }
+        },
+        methods: { //Aqui, a função nomeCompleto() será chamada toda vez que for usada no template, mesmo sem mudanças nos dados.
+            nomeCompletoMethodo() {
+                return `${this.primeiroNome} ${this.ultimoNome}`;
             }
         }
     }
---
+++ 2-functions/4-computed-getters-e-setters/src/components/operationComputed.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+
+
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'computedComponent',
+        data() {
+            return{
+
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+
+</style>
