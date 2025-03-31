118947ff8da132f00550cfc5885626202a84a4e1
philipheferreira <philipheferreira@gmail.com>
2025-03-31 09:11:19 -0300

    Feat: function 4 cap basic template for start

--- 2-functions/4-computed-getters-e-setters/src/components/operationComputed.vue
+++ 2-functions/4-computed-getters-e-setters/src/components/operationComputed.vue
@@ -1,17 +1,26 @@
+
 <template>
     <div>
-
-
+        <h3>Operação: </h3>
+        <p>Nome Completo pelo Computed: {{ nomeCompleto }}</p>
+        <p>Nome Completo pelo Method: {{ nomeCompletoMethodo }}</p>
 
     </div>
 </template>
 
 <script>
-    export default{
+
+    export default {
         name: 'computedComponent',
+        computed: {
+            nomeCompleto() {
+                return `${this.primeiroNome} ${this.ultimoNome}`
+            }
+        },
         data() {
             return{
-
+                primeiroNome: "Philiphe",
+                ultimoNome: "Ferreira"
             }
         }
     }
@@ -19,6 +28,7 @@
 </script>
 
 <style>
+
 
 
 </style>
