35fe8420548014131fdbc1b63d325f7e61477bab
philipheferreira <philipheferreira@gmail.com>
2025-04-05 11:38:08 -0300

    Feat: basic template components in clock emit project

---
+++ 3-projetos/5-clock-components-emit/src/components/minutos.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <div>
+
+        
+    </div>
+
+</template>
+
+<script>
+export default{
+    name: '',
+    data () {
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ 3-projetos/5-clock-components-emit/src/components/segundos.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div>
+
+
+        
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: '',
+        data () {
+            return{
+                
+            }
+        }
+    }
+</script>
+
+<style>
+
+
+</style>
