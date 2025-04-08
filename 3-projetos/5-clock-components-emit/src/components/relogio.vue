19cf9507421db1711705dd5452077a488643cfb6
philipheferreira <philipheferreira@gmail.com>
2025-04-08 14:59:19 -0300

    Feat: things i forget

--- 3-projetos/5-clock-components-emit/src/components/relogio.vue
+++ 3-projetos/5-clock-components-emit/src/components/relogio.vue
@@ -1,3 +1,8 @@
+<!-- Nesse codigo em vez de eu fazer uma conexão direta usando o Props e bind eu optei por usar uma conexão usando
+ metodos e chamadas pelo emit. Eu recebo os valores do emit utilizando a recepção de methodos pelo component 
+ Linkando um metodo do pai com um metodo do filho, e recebendo os valores dos metodos e repassando para
+ variaveis locais no component pai -->
+
 <template>
     <div>
         <componentHora  @atualizar-horas-local="atualizarHora" /> 
