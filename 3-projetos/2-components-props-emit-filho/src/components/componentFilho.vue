0d7fea8c0b2c69b3e5473717c498cde588548a51
philipheferreira <philipheferreira@gmail.com>
2025-04-11 00:47:07 -0300

    Feat: create basic template for component father and sun in project 2

---
+++ 3-projetos/2-components-props-emit-filho/src/components/componentFilho.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'componentFilho'
+        data (){
+            return{
+
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ 3-projetos/2-components-props-emit-filho/src/components/componentPai.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        Componente pai
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'componentPai',
+    data () {
+        return{
+
+        }
+    }
+}
+
+
+</script>
+
+<style>
+
+
+</style>
