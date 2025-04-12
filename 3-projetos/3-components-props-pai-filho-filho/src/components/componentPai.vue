967bbfd3ac869a526fe8a0f72ae8349fabd006d0
philipheferreira <philipheferreira@gmail.com>
2025-04-12 18:17:45 -0300

    Feat: module project cap 3 adjust component pai

---
+++ 3-projetos/3-components-props-pai-filho-filho/src/components/componentPai.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+
+    componente pai
+
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'componentPai',
+        data () {
+            return{
+
+            }
+        }
+    }
+
+</script>
+
+
+<style>
+
+
+</style>
