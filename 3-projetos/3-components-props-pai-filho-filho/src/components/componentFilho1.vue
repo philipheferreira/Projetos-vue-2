61f188e5bea2543297472f3bd9175e777864f94f
philipheferreira <philipheferreira@gmail.com>
2025-04-12 18:23:51 -0300

    Feat: things i forget

---
+++ 3-projetos/3-components-props-pai-filho-filho/src/components/componentFilho1.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    component filho 1
+</div>
+</template>
+
+<script>
+
+    export default {
+        name: '',
+        data () {
+            return {
+
+            }
+        }
+    }
+
+</script>
---
+++ 3-projetos/3-components-props-pai-filho-filho/src/components/componentFilho2.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    Component filho 2
+</div>
+</template>
+
+<script>
+
+export default {
+    name: '',
+    data () {
+        return{
+
+        }
+    }
+}
+
+</script>
+
+<style>
+
+
+</style>
