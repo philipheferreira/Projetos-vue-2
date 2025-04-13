ed150e87146d3ebe7fb6a38960e20d162288e1a8
philipheferreira <philipheferreira@gmail.com>
2025-04-13 10:02:48 -0300

    Feat: finish cap 2 in projects

--- 3-projetos/2-simple-button-props-validation/src/components/componentFilho.vue
+++ 3-projetos/2-simple-button-props-validation/src/components/componentFilho.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
-
+        {{ nomeLocal1 }}
+        {{ nomeLocal2 }}
+        {{ nomeLocal3 }}
+        {{ nomeLocal4 }}
     </div>
 </template>
 
 <script >
-
+    export default{
+        name: 'componentFilho',
+        props: {
+            nomeLocal1: [String, Array], // Defini que o props nomeLocal está esperando receber uma string ou um array. Se receber algo além disso retornará erro no console
+            nomeLocal2: {
+                type: String, // está esperando receber uma string
+                required: true // é um valor esperado, logo obrigatorio
+            },
+            nomeLocal3: {
+                type: String,
+                default: 'Anonimo' // quando um valor não for recebido nesse props ele ira retornar o valor dentro de anonimo na pagina
+            },
+            nomeLocal4: {
+                type: Number,
+                default: function() { // o default pode retornar funções também que funcionam repassando o valor para o props em questão
+                    return Array(10).fill(0).join(',')
+                }
+            }
+        }
+    }
 </script>
 
 <style scoped>
--- 3-projetos/2-simple-button-props-validation/src/components/componentPai.vue
+++ 3-projetos/2-simple-button-props-validation/src/components/componentPai.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-
+        <component-filho nomeLocal1="1" nomeLocal2="Philiphe Ferreira"/> <!-- Nome local está esperando receber array ou string, como está recebendo numero retornara erro -->
     </div>
 </template>
 
 <script >
+import componentFilho from './componentFilho'
     export default{
-        name: 'componentPai'
+        name: 'componentPai',
+        components: {componentFilho}
     }
 </script>
 
