34398b794204c7832b50917f7fca7297df475b53
philipheferreira <philipheferreira@gmail.com>
2025-04-22 19:00:42 -0300

    Feat: create basic project for slot named functions

---
+++ 3-projetos/5-simple-button-slot-named/src/components/ComponentPai.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <component-filho></component-filho>
+    </div>
+</template>
+
+<script>
+import ComponentFilho from './ComponentFilho.vue'
+export default {
+    name: 'ComponentPai',
+    components: {ComponentFilho}
+}
+</script>
+
+<style>
+
+</style>
