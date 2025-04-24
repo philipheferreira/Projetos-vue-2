48dd77b98a91722036ed41a6e449afde4a4d6c92
philipheferreira <philipheferreira@gmail.com>
2025-04-24 14:54:33 -0300

    Feat: adjust create basic emit and props comunication in event bus project button

---
+++ 3-projetos/004-simple-button-event-bus/src/components/ComponentFilho.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button @click="alterarNome">Alterar Nome</button>
+    </div>
+</template>
+
+<script>
+//import barramento from '@/barramento'
+export default {
+    name: 'componentFilho',
+    props: {
+        nomeLocal: String
+    },
+    methods:{
+        alterarNome(){
+            let nome = 'Isabella Siqueira Ferrreira'
+            this.$emit('comando-alterar-nome', nome)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 3-projetos/004-simple-button-event-bus/src/components/ComponentPai.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <component-filho :nomeLocal="nome" @comando-alterar-nome="nome = $event"/>
+        {{ nome }}
+    </div>
+</template>
+
+<script>
+import ComponentFilho from './ComponentFilho.vue'
+export default {
+    name: 'componentPai',
+    components: {
+        ComponentFilho
+    },
+    data () {
+        return {
+            nome: 'Philiphe Siqueira Ferreira'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
