43a7bbd78ea6c1ccc5d4fed6cd40d9c2a86ed2b9
philipheferreira <philipheferreira@gmail.com>
2025-04-29 14:59:17 -0300

    Feat: contador vuex project in main finish

--- 3-projetos/022-contador-components-vuex/src/components/contador.vue
+++ 3-projetos/022-contador-components-vuex/src/components/contador.vue
@@ -1,12 +1,28 @@
 <template>
     <div>
         Component Contador
+        <p>Contador: {{ contador }}</p>
+        <button @click="somarLocalmente">Somar</button>
+        <button @click="subtrairLocalmente">Subtrair</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'contadorComponent'
+    name: 'contadorComponent',
+    computed: {
+        contador() {
+            return this.$store.getters.contador
+        }
+    },
+    methods: {
+        somarLocalmente() {
+            this.$store.dispatch('somar')
+        },
+        subtrairLocalmente() {
+            this.$store.dispatch('subtrair')
+        }
+    }
 }
 </script>
 
--- 3-projetos/022-contador-components-vuex/src/main.js
+++ 3-projetos/022-contador-components-vuex/src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        contador: 0
+    },
+    mutations: {
+        somar(state) {
+            state.contador++
+        },
+        subtrair(state) {
+          state.contador--
+        }
+    },
+    actions: {
+        somar({ commit }) {
+            commit('somar') // o nome precisa ser o mesmo da mutation
+        },
+        subtrair({ commit }){
+          commit('subtrair')
+        }
+    },
+    getters: {
+        contador: state => state.contador
+    }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
