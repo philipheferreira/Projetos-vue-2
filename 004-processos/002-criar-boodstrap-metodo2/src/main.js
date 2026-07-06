6c274a8a629558b223fa8d630c02af1c852eb7c0
philipheferreira <philipheferreira@gmail.com>
2026-07-06 11:16:02 -0300

    correction in boodstrap

--- 004-processos/002-criar-boodstrap-metodo2/src/main.js
+++ 004-processos/002-criar-boodstrap-metodo2/src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 1. Importe o BootstrapVue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// 2. Importe os arquivos CSS (do Bootstrap normal e do BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// 3. Torne o BootstrapVue disponível no projeto
+Vue.use(BootstrapVue)
+// Opcional: Plugin de ícones
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
