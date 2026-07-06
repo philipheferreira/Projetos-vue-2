d3e46acee01b7b2121b2e3752fc89ce45b9d7532
philipheferreira <philipheferreira@gmail.com>
2026-07-06 11:06:14 -0300

    Feat: create basic process for bodstrap and tailwind

---
+++ 004-processos/001-criar-boodstrap-metodo1/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container mt-5">
+      <div class="alert alert-success">
+        Bootstrap funcionando no Vue 2!
+      </div>
+      <button class="btn btn-primary">Meu Botão</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
