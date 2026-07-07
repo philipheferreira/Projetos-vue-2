48cb44d68dc09abbdfe85bd828d4ebf33f0e1dd4
philipheferreira <philipheferreira@gmail.com>
2026-07-07 12:23:54 -0300

    Feat: basic project test

---
+++ 005 - testes/teste-componentes/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <contadores></contadores>
+  </div>
+</template>
+
+<script>
+import Contadores from './components/Contadores.vue'
+export default {
+  name: 'App',
+  components: {Contadores}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 005 - testes/teste-componentes/src/components/Contadores.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="contadores">
+        <app-contador v-for="c in 5" :key="c"></app-contador>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ContadoresOperacao',
+
+    }
+</script>
+
+<style scoped>
+</style>
