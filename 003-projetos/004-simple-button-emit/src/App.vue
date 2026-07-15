c66d3a7d6cc0f3990ff9e05f2b2ecb480198e677
philipheferreira <philipheferreira@gmail.com>
2026-07-15 11:15:14 -0300

    create project emit

---
+++ 003-projetos/004-simple-button-emit/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ComponentFilho></ComponentFilho>
+  </div>
+</template>
+
+<script>
+import ComponentFilho from './components/componentFilho.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ComponentFilho
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 003-projetos/004-simple-button-emit/src/components/componentFilho.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Sou o component filho
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'componentFilho'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
