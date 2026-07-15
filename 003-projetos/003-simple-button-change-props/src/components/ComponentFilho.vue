4f4c7323b3ca7e5869490c7ec3ab6cd6f83d7bf9
philipheferreira <philipheferreira@gmail.com>
2026-07-15 11:52:31 -0300

    Corrections in operations vue change props

---
+++ 003-projetos/003-simple-button-change-props/src/components/ComponentFilho.vue
@@ -0,0 +1,30 @@
+/* 
+A Regra de Ouro: Fluxo de Dados Unidirecional
+No Vue 2, uma prop nunca deve ser alterada pelo componente filho. Se o filho tentar fazer this.mensagem = 'Novo texto', o Vue emitirá um aviso no console.
+
+Por que essa regra existe?
+Para evitar que o filho modifique o estado do pai de forma imprevisível, o que dificulta muito o rastreamento de bugs.
+
+Como resolver se eu precisar alterar o dado?
+Você tem duas opções principais no Vue 2:
+
+Opção A: Usar uma variável local (Data) baseada na prop
+
+Opção B: Usar uma Prop como valor inicial e computá-la (Computed)
+*/
+
+<template>
+    <div>
+        Eu sou o component filho
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'componentFilho'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
